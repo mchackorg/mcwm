fcaa33e8845c2ae8d2b3775d06a0dddaa0888faf
Michael Cardell Widerkrantz <mc@hack.org>
2011-03-18 09:16:05 +0100

    *** empty log message ***

--- config.h
+++ config.h
@@ -8,8 +8,13 @@
 
 /*
  * Use this modifier combined with other keys to control wm from
- * keyboard. Default is Mod4, which on my keyboard is the Alt/Windows
- * key.
+ * keyboard. Default is Mod4, which on my keyboard is the Alt key but
+ * is usually the Windows key on more normal keyboard layout.
+ *
+ * NOTA BENE! Also remember to define the key that generates this
+ * modifier as USERKEY_MOD below, otherwise the MODKEY + Tab function
+ * won't work properly. This is a bit of a kluge, I know, and will be
+ * fixed.
  */
 #define MODKEY XCB_MOD_MASK_4
 
