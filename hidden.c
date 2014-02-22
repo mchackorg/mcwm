31cdcb57d2eb94c796b4d9931c8cf8f750d2d1a8
Michael Cardell Widerkrantz <mc@hack.org>
2014-02-22 17:39:32 +0100

    "and" -> "and/or".

--- hidden.c
+++ hidden.c
@@ -5,9 +5,10 @@
  * Copyright (c) 2012 Michael Cardell Widerkrantz, mc at the domain
  * hack.org.
  *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose with or without fee is hereby granted, provided
+ * that the above copyright notice and this permission notice appear
+ * in all copies.
  *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
